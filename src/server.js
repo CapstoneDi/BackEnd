30cb624d1027259f8e45a27ac6b7c14137dac435
Harry0817-code <harrynugrohosusetyo@gmail.com>
2025-07-30 00:46:50 +0700

    Modified: add port

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ const init = async () => {
   }
 
   const server = Hapi.server({
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
     host: '0.0.0.0',
     routes: {
       cors: {
